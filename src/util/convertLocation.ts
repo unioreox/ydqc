c099b1ed59a5e66d5a68dfe4211c5faa13bc94a3
SteamFinder-409 <steamfinder@foxmail.com>
2025-04-16 21:46:29 +0800

    test: change cl

--- src/util/convertLocation.ts
+++ src/util/convertLocation.ts
@@ -48,7 +48,7 @@ export function wgs84ToGcj02(latitude:any , longitude: any) {
     }
 
     // return wgs84 ? `${wgs84[0]},${wgs84[1]}` : '';
-    return [gcj02[0], gcj02[1]];
+    return [gcj02[0].toFixed(6), gcj02[1].toFixed(6)];
 }
 
 // function Empty() {
